592b9b5d0a77c1cbdb16ee8a50cee1f1d20a6a2c
io37@njit.edu <Wendy#@1>
2019-04-22 19:38:43 -0400

    Adding the Update User Test

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use \App\User;
+
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+
+    	$user = Users::find(51);
+    	$user->name = 'Steve Smith';
+    	$user->save();
+    	
+    	$result = (isset($user->name) && $user->name == 'Steve Smith' )?true:false;
+        $this->assertTrue($result);
+
+    }
+}
