8ae86e1753326835dca127fafca6ec632ed32fdd
io37@njit.edu <Wendy#@1>
2019-04-21 22:13:52 -0400

    Adding the car model and populationg the table

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    return [
+        'make'  => $faker->randomElement([ 'ford', 'honda', 'toyota' ]),
+        'model' => $faker->word(),
+        'year'  => $faker->year(),
+
+    ];
+});
